39a85a9b07df818ed3a2444a4f04d239b742c8c5
IceYoshi <mike_pereira100000@yahoo.de>
2018-01-07 22:16:03 +0100

    Add program options

--- src/rng_mpi.cpp
+++ src/rng_mpi.cpp
@@ -8,8 +8,11 @@
 #include <boost/mpi/environment.hpp>
 #include <boost/mpi/communicator.hpp>
 
+#include <boost/program_options.hpp>
+
 #include "Generator.hpp"
 
+namespace po = boost::program_options;
 namespace mpi = boost::mpi;
 using namespace std;
 
@@ -29,21 +32,64 @@ int main(int argc, char *argv[]) {
 
 	id = world.rank();
 
+	unsigned long n;
+	unsigned int g;
+	bool h = false;
+
+	if (id == 0) {
+		po::options_description desc("Available options");
+		// supported options
+		desc.add_options()
+			("help,h", "Display this help message")
+			("streamlength,n", po::value<unsigned long>()->default_value(0), "Number of bytes to be generated. Set to 0 for infinite execution.")
+			("generator,g", po::value<unsigned int>()->default_value(0), "Type of generator. 0 for rand(), 1 for mt19937")
+			;
+
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm); //assign the variables (if they were specified)
+
+		if (vm.count("help")) {
+			cout << desc << endl;
+			h = true;
+		}
+
+		n = vm["streamlength"].as<unsigned long>();
+		g = vm["generator"].as<unsigned int>();
+	}
+
 	world.barrier();
+
+	mpi::broadcast(world, n, 0);
+	mpi::broadcast(world, g, 0);
+	mpi::broadcast(world, h, 0);
+
+	if (h) return 1;
 
 	//-----------------------------------------------
 	unsigned int bitSize = 16;
 
-	// boost::mt19937 wrapper
-	Random::Generator<int> g(0, (1 << bitSize) - 1, id);
-
 	// Initialize seed for rand()
-	//srand(time(NULL));
+	srand(time(NULL));
 
-	while (true) {
+	// boost::mt19937 wrapper
+	Random::Generator<int> mt(0, (1 << bitSize) - 1);
+
+	unsigned long count = 0;
+	int x;
+	while (n == 0 || count++ < n) {
 		// Generate random number between 0 and 2^16 - 1 (25535)
-		//int x = rand() % (1 << bitSize);
-		int x = g();
+		switch (g) {
+		case 0: // rand()
+			x = rand() % (1 << bitSize);
+			break;
+		case 1:	// mt19937
+			x = mt();
+			break;
+		default:
+			cout << "Error: Undefined generator " << g << endl;
+			return 2;
+		}
 
 		//Print out the raw binary representation of the generated number
 		cout << (char)((0xff00 & x) >> 8);
--- src/rng_seq.cpp
+++ src/rng_seq.cpp
@@ -3,26 +3,60 @@
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
 
+#include <boost/program_options.hpp>
+
 #include "Generator.hpp"
 
+namespace po = boost::program_options;
 using namespace std;
 
 /**
 * Generate a continuous raw binary stream
 */
 int main(int argc, char * argv[]){
-	unsigned int bitSize = 16;
+	po::options_description desc("Available options");
+	// supported options
+	desc.add_options()
+		("help,h", "Display this help message")
+		("streamlength,n", po::value<unsigned long>()->default_value(0), "Number of bytes to be generated. Set to 0 for infinite execution.")
+		("generator,g", po::value<unsigned int>()->default_value(0), "Type of generator. 0 for rand(), 1 for mt19937")
+		;
 
-	// boost::mt19937 wrapper
-	//Random::Generator<int> g(0, (1 << bitSize) - 1);
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm); //assign the variables (if they were specified)
+
+	if (vm.count("help")) {
+		cout << desc << endl;
+		return 1;
+	}
+
+	unsigned long n = vm["streamlength"].as<unsigned long>();
+	unsigned int g = vm["generator"].as<unsigned int>();
+
+	unsigned int bitSize = 16;
 
 	// Initialize seed for rand()
 	srand(time(NULL));
 
-	while (true) {
+	// boost::mt19937 wrapper
+	Random::Generator<int> mt(0, (1 << bitSize) - 1);
+
+	unsigned long count = 0;
+	int x;
+	while (n == 0 || count++ < n) {
 		// Generate random number between 0 and 2^16 - 1 (25535)
-		int x = rand() % (1 << bitSize);
-		//int x = g();
+		switch (g) {
+		case 0: // rand()
+			x = rand() % (1 << bitSize);
+			break;
+		case 1:	// mt19937
+			x = mt();
+			break;
+		default:
+			cout << "Error: Undefined generator " << g << endl;
+			return 2;
+		}
 		
 		//Print out the raw binary representation of the generated number
 		cout << (char)((0xff00 & x) >> 8);
