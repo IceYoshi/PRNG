7441d0fd1cb80ab846e08fa3a522c42d7eaf96ad
IceYoshi <mike_pereira100000@yahoo.de>
2018-01-25 22:25:31 +0100

    Add guide to install Dieharder on HPC Platform

---
+++ src/dieharder-info/dieharder-install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mkdir ~/stow
+
+wget https://webhome.phy.duke.edu/~rgb/General/dieharder/dieharder-3.31.1.tgz
+tar xvzf dieharder-3.31.1.tgz
+cd dieharder-3.31.1/
+
+module load toolchain/foss
+module load numlib/GSL/2.3-foss-2017a
+
+./configure --prefix=$HOME/stow/dieharder-3.31.1
+
+make
+make install
+
+cd ~/stow
+stow dieharder-3.31.1/
