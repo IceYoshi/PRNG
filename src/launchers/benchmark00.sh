16d0e69fc9d90d87e515a1ad0e015f5b86576fc3
IceYoshi <mike_pereira100000@yahoo.de>
2018-01-25 22:24:12 +0100

    Add mpi speed benchmark launchers

---
+++ src/launchers/benchmark00.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+#SBATCH -J PRNGBenchmark1
+#SBATCH --mail-type=end,fail
+#SBATCH --mail-user=mike.pereira.001@student.uni.lu
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=28
+#SBATCH --time=0-01:00:00
+#SBATCH -p batch
+#SBATCH --qos=qos-batch
+
+module purge
+module load toolchain/foss
+module load devel/Boost/1.65.1-foss-2017a
+
+cd ~/PRNG/build
+
+echo "Start of benchmarking with g=0"
+
+time mpirun -np   1 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np   5 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np  10 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np  15 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np  20 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np  25 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np  28 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+
+echo "End of benchmarking"
---
+++ src/launchers/benchmark01.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+#SBATCH -J PRNGBenchmark1
+#SBATCH --mail-type=end,fail
+#SBATCH --mail-user=mike.pereira.001@student.uni.lu
+#SBATCH -N 3
+#SBATCH --ntasks-per-node=28
+#SBATCH --time=0-01:00:00
+#SBATCH -p batch
+#SBATCH --qos=qos-batch
+
+module purge
+module load toolchain/foss
+module load devel/Boost/1.65.1-foss-2017a
+
+cd ~/PRNG/build
+
+echo "Start of benchmarking with g=0 (cont.)"
+
+time mpirun -np 28 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np 30 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np 35 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np 40 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np 45 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np 50 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np 55 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+time mpirun -np 56 ./rng_mpi -g 0 -m 0 -n 100000000000 &> /dev/null
+
+echo "End of benchmarking"
