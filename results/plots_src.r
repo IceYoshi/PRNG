767341a47a3f1a927b301ef9e57aea6a4d1d2562
IceYoshi <mike_pereira100000@yahoo.de>
2018-01-26 12:29:47 +0100

    Add result plots

--- results/plots_src.r
+++ results/plots_src.r
@@ -1,44 +1,26 @@
-par(mfrow=c(1,3)) # Show all graphs simultaneously in a row
-
-# Execution speed comparison between differeng RNGs (sequential)
-x = c(1000,10000,100000,1000000,10000000,100000000,1000000000,10000000000)
-t0 = c(21,23,42,158,978,9175,91271,912230)
-t1 = c(21,22,41,156,953,8924,88389,883718)
-t2 = c(20,23,45,181,1145,10855,108012,1079419)
-
-plot(x, t0, log="xy", type="o", pch=19, las=0, ylim=c(20, 1200000), xlim=c(1000,10000000000), 
-    main="Execution speed comparison between differeng RNGs (sequential)", 
-    xlab="Number of generated random 32-bit numbers (log scale)", 
-    ylab="Execution time in ms (log scale)"
-)
-
-lines(x, y=t1, col="red", type="o", pch=6)
-lines(x, y=t2, col="blue", type="o", pch=8)
-
-grid(col = "lightgray", lty = "dotted", lwd = par("lwd"), equilogs = TRUE)
-legend(1000, 1200000, legend=c("rand()", "mt19937", "MRG32k3a"), col=c("black", "red", "blue"), pch=c(19,6, 8))
-
 #-----------------------------------------------------------------------------------------------------------------
-# Execution speed comparison between differeng RNGs (OpenMPI) with constant m = 0 and n = 100000000
-x = c(1,5,10,15,20)
-t0 = c(49229,8409,4202,2652,2028)
-t1 = c(39214,7316,3329,2542,1915)
-t2 = c(39066,8318,3932,2448,1873)
+# Execution speed comparison between differeng RNGs (OpenMPI) with constant m = 0 and n = 100000000000
+x = c(1,5,10,15,20, 25, 28, 28, 30, 35, 40, 45, 50, 55, 56)
+t0 = c(1647.502,353.919,187.934,125.536,94.402,75.641,68.281,68.617,64.288,55.046,48.566,43.301,39.051,35.734,35.4)
+t1 = c(491.299,105.46,56.2,37.605,28.347,22.793,20.696,41.57,38.667,34.747,29.273,26.085,23.721,22.92,22.471)
+t2 = c(2420.335,519.241,276.556,200.97,200.315,198.732,201.233,102.202,96.672,81.1,72.419,63.716,58.615,52.125,52.539)
 
-plot(x, t0, log="y", type="o", pch=19, las=0, ylim=c(1500, 55000), xlim=c(1,20), 
-    main="Execution speed comparison between differeng RNGs (OpenMPI) \n with constant m = 0 and n = 100000000",
+plot(x, t0, log="y", type="o", pch=19, las=0, ylim=c(20, 2500), xlim=c(1,56), 
+    main="Execution speed comparison between differeng RNGs (OpenMPI) \n with constant m = 0 and n = 100000000000",
     xlab="Number of parallel processes", 
-    ylab="Execution time in ms (log scale)"
+    ylab="Execution time in s (log scale)"
 )
 
 lines(x, y=t1, col="red", type="o", pch=6)
 lines(x, y=t2, col="blue", type="o", pch=8)
 
 grid(col = "lightgray", lty = "dotted", lwd = par("lwd"), equilogs = TRUE)
-legend(15.5, 55000, legend=c("rand()", "mt19937", "MRG32k3a"), col=c("black", "red", "blue"), pch=c(19,6, 8))
+legend(44, 2500, legend=c("rand()", "mt19937", "MRG32k3a"), col=c("black", "red", "blue"), pch=c(19,6, 8))
+
+abline(v = 28, col="black")
 
 #-----------------------------------------------------------------------------------------------------------------
-# Execution speed comparison between differeng parallelism methods for mt19937 generator and n = 100000000
+# Execution speed comparison between differeng parallelism methods for mt19937 generator and n = 100000000000
 x = c(1,5,10,15,20)
 t0 = c(39214,7316,3329,2542,1915)
 t1 = c(41023,8568,3669,2346,2041)
---
+++ src/launchers/mode/mode10.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+#SBATCH -J PRNGBenchmark1
+#SBATCH --mail-type=end,fail
+#SBATCH --mail-user=mike.pereira.001@student.uni.lu
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=28
+#SBATCH --time=0-02:00:00
+#SBATCH -p batch
+#SBATCH --qos=qos-batch
+
+module purge
+module load toolchain/foss/2017a
+module load devel/Boost/1.65.1-foss-2017a
+
+cd ~/PRNG/build
+
+echo "Start of mode test with m=1"
+
+time mpirun -np   1 ./rng_mpi -g 1 -m 1 -n 100000000000 &> /dev/null
+time mpirun -np   5 ./rng_mpi -g 1 -m 1 -n 100000000000 &> /dev/null
+time mpirun -np  10 ./rng_mpi -g 1 -m 1 -n 100000000000 &> /dev/null
+time mpirun -np  15 ./rng_mpi -g 1 -m 1 -n 100000000000 &> /dev/null
+time mpirun -np  20 ./rng_mpi -g 1 -m 1 -n 100000000000 &> /dev/null
+time mpirun -np  25 ./rng_mpi -g 1 -m 1 -n 100000000000 &> /dev/null
+time mpirun -np  28 ./rng_mpi -g 1 -m 1 -n 100000000000 &> /dev/null
+
+echo "End of benchmarking"
